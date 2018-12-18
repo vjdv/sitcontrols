9a66cfc8920161965f611e309ac237f8c965a08b
Jassiel <vdiaz@elektra.com.mx>
2018-12-18 15:07:17 -0600

    state now is based in value instead selectedindex

--- src/components/inputs/inputoption.js
+++ src/components/inputs/inputoption.js
@@ -8,10 +8,10 @@ export default class InputOption extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: false,
+      focused: false,
+      showList: false,
       text: "",
       filteredOptions: [],
-      selectedIndex: -1,
       pickedIndex: 1
     };
     this.labelField = props.labelField || "label";
@@ -20,21 +20,20 @@ export default class InputOption extends React.Component {
   }
   render() {
     const parentStyle = { width: this.props.width, ...this.props.style };
-    const inputStyle = { color: this.selectedIndex === -1 ? "#888" : undefined };
     return (
-      <div className={cx(s2.inputoption, this.state.selectedIndex !== -1 && s2.choiced)} style={parentStyle}>
+      <div className={s2.inputoption} style={parentStyle}>
         <input
           type="search"
-          className={s1.sitcontrol}
-          style={inputStyle}
+          ref={o => (this.input = o)}
+          className={cx(s1.sitcontrol, this.state.value === "" && s2.nochoiced)}
           value={this.text}
           onChange={this.changeHandler}
           onKeyDown={this.keyHandler}
-          onFocus={this.showOptions}
-          onBlur={this.hideOptions}
+          onFocus={this.focusHandler}
+          onBlur={this.blurHandler}
         />
-        <input type="hidden" name={this.props.name} value={this.value} />
-        {this.state.show && (
+        {this.props.name !== undefined && <input type="hidden" name={this.props.name} value={this.value} />}
+        {this.state.showList && (
           <List
             className={s2.list}
             width={this.props.width || 300}
@@ -49,80 +48,107 @@ export default class InputOption extends React.Component {
     );
   }
   get value() {
-    return this.state.selectedIndex === -1 ? "" : this.selectedItem[this.valueField] || "";
+    return this.state.value;
   }
   set value(val) {
-    const findings = this.props.options.filter(o => o[this.valueField] === val);
-    if (findings.length === 0) this.setState({ selectedIndex: -1 });
-    else this.setState({ text: findings[0][this.labelField], value: val });
+    if (this.props.value !== undefined) return console.warn("value already set in props");
+    var item = null;
+    for (var i = 0; i < this.props.options.length; i++) {
+      if (this.props.options[i][props.valueField] === val) {
+        item = this.props.options[i];
+        break;
+      }
+    }
+    this.setState({ value: item === null ? "" : item[this.props.valueField], text: item === null ? "" : item[this.props.labelField] });
   }
   get text() {
-    return this.state.selectedIndex === -1 ? this.state.text : this.selectedItem[this.labelField];
+    const item = this.selectedItem;
+    return this.state.focused ? this.state.text : item === null ? "" : item[this.props.labelField];
   }
   get selectedItem() {
-    return this.props.options[this.state.selectedIndex] || null;
-  }
-  set selectedItem(item) {
-    const index = this.props.options.indexOf(item);
-    this.setState({ selectedIndex: index });
+    const index = this.selectedIndex;
+    return index === -1 ? null : this.props.options[index];
   }
   get selectedIndex() {
-    return this.state.selectedIndex;
+    for (var i = 0; i < this.props.options.length; i++) {
+      if (this.props.options[i][this.props.valueField] === this.state.value) return i;
+    }
+    return -1;
   }
-  set selectedIndex(index) {
-    this.setState({ selectedIndex: index });
+  static getDerivedStateFromProps(props, state) {
+    const newstate = {};
+    if (props.value !== undefined) {
+      var item = null;
+      for (var i = 0; i < props.options.length; i++) {
+        if (props.options[i][props.valueField] === props.value) {
+          item = props.options[i];
+          break;
+        }
+      }
+      state.value = item === null ? "" : props.value;
+    }
+    return newstate;
   }
   changeHandler = e => {
     const text = e.target.value;
-    var selectedIndex = text === this.text ? this.state.selectedIndex : -1;
-    this.setState({ text, selectedIndex }, this.filterOptions);
+    this.setState({ text, showList: true, pickedIndex: 0 }, this.filterOptions);
   };
-  changeChecker = prevValue => {
-    if (this.onChange && prevValue !== this.value) this.onChange({ target: this, oldValue: prevValue, newValue: this.value });
+  changeTester = (oldValue, newValue) => {
+    if (this.props.onChange !== undefined && oldValue !== newValue) this.props.onChange({ target: this, oldValue, newValue });
   };
   clickHandler = e => {
-    const previous = this.state.selectedIndex;
-    this.setState({ selectedIndex: Number(e.target.dataset.index) }, () => this.changeChecker(previous));
+    const oldValue = this.state.value;
+    const newIndex = Number(e.target.dataset.index);
+    const newValue = this.state.filteredOptions[newIndex][this.props.valueField];
+    this.setState({ value: newValue }, () => this.changeTester(oldValue, newValue));
   };
   filterOptions = () => {
-    var filteredOptions = this.props.options.filter((o, i) => {
-      o.sit_fid = i;
-      return this.state.text === "" || o[this.labelField].toLowerCase().indexOf(this.state.text.toLowerCase()) !== -1;
-    });
-    const previous = this.state.selectedIndex;
-    this.setState({ filteredOptions, show: true, value: "" }, () => this.changeChecker(previous));
+    const item = this.selectedItem;
+    const showAll = this.state.text === "" || this.state.text === item[this.props.labelField];
+    const textLowered = this.state.text.toLowerCase();
+    const filteredOptions = this.props.options.filter(o => showAll || o[this.labelField].toLowerCase().indexOf(textLowered) !== -1);
+    this.setState({ filteredOptions });
   };
-  hideOptions = () => {
-    setTimeout(() => this.setState({ show: false, text: "" }), 150);
-  };
+  hideOptions = () => setTimeout(() => this.setState({ showList: false }), 200);
   keyHandler = e => {
     const key = e.keyCode;
     if (key === 13 || key === 38 || key === 40) {
       e.preventDefault();
       if (this.state.filteredOptions.length === 0) return;
-      if (key === 40 && this.state.pickedIndex + 1 < this.state.filteredOptions.length) {
+      if ((key === 38 || key === 40) && this.state.showList === false) {
+        this.focusHandler();
+      } else if (key === 40 && this.state.pickedIndex + 1 < this.state.filteredOptions.length) {
         this.setState({ pickedIndex: this.state.pickedIndex + 1 });
       } else if (this.state.pickedIndex === -1 && key === 38) {
-        this.setState({ pickedIndex: this.state.filteredOptions.length - 1, show: true });
+        this.setState({ pickedIndex: this.state.filteredOptions.length - 1, showList: true });
       } else if (key === 38 && this.state.pickedIndex > 0) {
         this.setState({ pickedIndex: this.state.pickedIndex - 1 });
-      } else if (key === 13 && this.state.show) {
-        const obj = this.state.filteredOptions[this.state.pickedIndex];
-        const prevValue = this.value;
-        this.setState({ text: obj[this.labelField], selectedIndex: obj.sit_fid, value: obj[this.valueField], show: false }, () => this.changeChecker(prevValue));
+      } else if (key === 13 && this.state.showList) {
+        const oldValue = this.state.value;
+        const newItem = this.state.filteredOptions[this.state.pickedIndex];
+        const newValue = newItem[this.props.valueField];
+        this.setState({ text: newItem[this.props.labelField], value: newValue, showList: false }, () => this.changeTester(oldValue, newValue));
       }
     }
   };
-  showOptions = () => {
-    const pickedIndex = this.state.selectedIndex === -1 ? 0 : this.state.selectedIndex;
-    this.setState({ show: true, pickedIndex }, this.filterOptions);
+  focusHandler = () => {
+    const index = this.selectedIndex;
+    const pickedIndex = index || 0;
+    const text = index === -1 ? "" : this.props.options[index][this.props.labelField];
+    this.setState({ focused: true, showList: true, pickedIndex, text }, () => {
+      this.input.select();
+      this.filterOptions();
+    });
+  };
+  blurHandler = () => {
+    this.setState({ focused: false, text: "" }, this.hideOptions);
   };
   renderListItem = ({ index, key, style }) => {
     const o = this.state.filteredOptions[index];
     const picked = this.state.pickedIndex === index;
-    const selected = this.state.selectedIndex === o.sit_fid;
+    const selected = this.state.value === o[this.props.valueField];
     return (
-      <div className={cx(s2.item, picked && s2.picked, selected && s2.selected)} data-index={o.sit_fid} key={key} style={style} onClick={this.clickHandler}>
+      <div className={cx(s2.item, picked && s2.picked, selected && s2.selected)} data-index={index} key={key} style={style} onClick={this.clickHandler}>
         {o[this.labelField]}
       </div>
     );
